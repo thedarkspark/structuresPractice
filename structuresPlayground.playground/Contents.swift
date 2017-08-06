7388166d0f050154ec51674511f59cb3449c3e00
thedarkspark <Dwaynebweaver@gmail.com>
2017-08-06 12:32:36 -0500

    structure chaptegr
    Practice the use of structures

---
+++ structuresPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+
+
+//Instances pg 134
+
+struct Shirt {
+    var size: Size
+    var color: Color
+}
+
+
+// Defines attributes of the shirt
+
+let myShirt = Shirt (size: .xl, color: .blue)
+//Creates an instance of one shirt
+
+let yourShirt = Shirt (size:.m, color:.red)
+// size and color are undeclared? How?
+//DONT KNOW WHY IM GETTING COMPILER ERROR HERE!?!?!
+//Moving right along though
+
+//Initializers "init()" pg 135
+//Give the default value of zero and false
+
+var string = String.init() // ""
+var integer = int.init() // 0
+var bool = Bool.init() // false
+
+
+
+
+
+
+
+
+
+
+
+
+
