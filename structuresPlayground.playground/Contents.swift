96631655144ccae934f04d457bf27d8856da107a
thedarkspark <Dwaynebweaver@gmail.com>
2017-08-13 22:36:02 -0500

    for learning purposes
    applying knowledge

--- structuresPlayground.playground/Contents.swift
+++ structuresPlayground.playground/Contents.swift
@@ -2,29 +2,57 @@
 
 import UIKit
 
-//Structures pg 132
+//This should be fun
 
-struct Person{
+//Structures
+//these are variables/properties of "Human"
+struct Human {
+    var numberOfLegs : Int
+    var numberOfFingers : Int
     var name : String
-    func sayWhatUp() {
-        print ("What up, my name is \(name)")
+    //now lets add a function to this struct
+    func sayName() {
+        print("My name is \(name) and I have \(numberOfLegs) legs")  //now every instance has this function
     }
 }
-let firstPerson = Person(name:"Master Tech")
-print(firstPerson.name)
-firstPerson.sayWhatUp()
+
+//Now add an object/instance of that structure above
+
+var Bruce = Human(numberOfLegs:2, numberOfFingers:10, name:"Bruce")
+Bruce.name
+Bruce.numberOfLegs
+Bruce.numberOfFingers
+//function time!! lol
+Bruce.sayName()
+
+//another instance
+
+var Alfred = Human(numberOfLegs: 2, numberOfFingers: 10, name: "Alfred The Butler")
+Alfred.name
+Alfred.numberOfFingers
+Alfred.numberOfLegs
+Alfred.sayName()
+
+
+//Example 2, finding the volume of a box
+
+struct Box {
+    //ahh a different way to write variable/properties using the comma. I learn something new everyday
+    var width : Double , height : Double , depth : Double
+    //now lets tell our program how to calculate volume by using "return"
+    var volume : Double {
+        return width * height * depth
+}
+}
+
+let fourByFiveByTwo = Box(width: 4, height: 5, depth: 2)
+fourByFiveByTwo.volume
+
+let fourByFiveByThree = Box(width: 4, height: 5, depth: 3)
+fourByFiveByThree.volume
 
 
 
-
-//Instances pg 134
-
-//Initializers "init()" pg 135
-//Give the default value of zero and false
-
-var string = String.init() // ""
-var integer = int.init() // 0
-var bool = Bool.init() // false
 
 
 
